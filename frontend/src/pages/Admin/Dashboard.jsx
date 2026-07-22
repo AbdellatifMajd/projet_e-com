79b06e8841c4fbadbb6b2edcbd43be3ba4beef6a
AbdellatifMajd7 <AbdellatifMajd10@gmail.com>
2026-07-22 18:26:55 +0100

    fix: resolution des conflits de merge frontend

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, laudantium.</div>
     <div className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, laudantium.</div>
   )
 }
